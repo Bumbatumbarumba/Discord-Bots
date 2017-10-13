cbea9a2711389b1e13d62876537587e246364c4b
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2017-10-13 13:53:44 -0400

    added help command to explain what the bot does

--- magic_conch.py
+++ magic_conch.py
@@ -27,9 +27,24 @@ async def on_ready():
 async def on_message(message):
 	if message.content.startswith("!conch"):
 		conch_answer = sample(answers, 1);
-		await client.send_message(message.channel, "\:shell:" + str(conch_answer));
+		await client.send_message(message.channel, ":shell: " + str(conch_answer));
 
 	if message.content.startswith("!cmdhelp"):
-		await client.send_message(message.channel, "Commands: !conch");
+		await client.send_message(message.channel, """----COMMANDS LIST----\n
+			General:\n
+			\t!whomst - if you want to more about the bot\n
+			Magic Conch:\n
+			\t!conch <question> - ask the magic conch a question\n
+			Gucci Points:\n
+			\t WORK IN PROGRESS, DOES NOT WORK\n
+			\t!gpwhatis - explains what Gucci Points are\n
+			\t!gpadd <user> - add a user that can receive Gucci Points\n
+			\t!gpgive <user> <integer> - give a user Gucci Points\n
+			\t!gprank - lists all users by Gucci Points\n
+			\t!gplist - lists all users who can gain Gucci Points\n
+			Quotes:\n
+			\t WORK IN PROGRESS, DOES NOT WORK\n
+			\t!quote <quote> - did someone say something dumb? Immortalize it!\n
+			\t!randquote - pulls a random quote that was added""");
 
 client.run('MzY4MjgwNzA1MzgzMzk5NDI0.DMJyoQ.dnm19X8QZ7Ibl_5RD-a1cAQT9Ms');
